8d4cef11941b7ec5f0b0314287d0d23843d51f26
Sourav Kayal <sourav.kayal@yahoo.com>
2024-03-16 09:09:47 +0530

    initial code
    initial code

--- tflab.tf
+++ tflab.tf
@@ -2,10 +2,10 @@ terraform {
 
 
   required_version = ">=0.12"
-  
+
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~>2.0"
     }
   }
@@ -18,6 +18,6 @@ provider "azurerm" {
 
 
 resource "azurerm_resource_group" "rg" {
-  name      = "MyTF3RG"
-  location  = "australiaeast"
+  name     = "MyTF3RG"
+  location = "australiaeast"
 }
